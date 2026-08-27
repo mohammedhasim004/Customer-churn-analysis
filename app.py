341f7515aafc7f5b9e646033b1958d9b9e1d80e8
mohammedhasim004 <hx.hasim.md@gmail.com>
2026-08-27 10:48:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ import streamlit as st
 st.title("📊 Customer Churn Predictor")
 
 tenure = st.slider("Tenure (Months)", 1, 72, 12)
-monthly_charges = st.number_input("Monthly Charges ($)", 10.0, 150.0, 65.0)
+monthly_charges = st.number_input("Monthly Charges (₹)", 100.0, 15000.0, 5000.0)
 
-if st.button("Predict"):
-    if monthly_charges > 70 and tenure < 12:
-        st.error("⚠️ Customer will Churn (Leave)")
+    if tenure < 12 and monthly_charges > 60:
+        st.error("⚠️ Customer will Churn (Leave) - New customer with high charges")
+    elif tenure >= 12 and monthly_charges > 110:
+        st.error("⚠️ Customer will Churn (Leave) - Old customer with excessive charges")
     else:
         st.success("✅ Customer will Stay")
